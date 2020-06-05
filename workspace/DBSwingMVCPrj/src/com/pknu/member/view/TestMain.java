6584163791833beab3589d222e36b83381adb7c9
sh6137 <33920015+sh6137@users.noreply.github.com>
2020-06-05 09:16:53 +0900

    aa

--- workspace/DBSwingMVCPrj/src/com/pknu/member/view/TestMain.java
+++ workspace/DBSwingMVCPrj/src/com/pknu/member/view/TestMain.java
@@ -1,22 +1,40 @@
 package com.pknu.member.view;
 
 import java.util.List;
+import java.util.Vector;
 
 import com.pknu.member.dao.MemberDao;
 import com.pknu.member.vo.MemberVO;
 
-public class TestMain {
+public abstract class TestMain {
+
 	public static void main(String[] args) {
-		//개인조회
-		MemberDao mDao = new MemberDao();
-		MemberVO mem = mDao.getMember("seal");
-		System.out.println(mem.info());
+	
 		
-		//목록조회
-		System.out.println("목록조회 : ");
-		List<MemberVO> mList = mDao.getMemberList("가");
-		for(MemberVO memberVO : mList) {
-			System.out.println(memberVO.info());
+		// 개인조회
+		MemberDao      mDao  =  new MemberDao();
+		MemberVO       mem   =  mDao.getMember("sea1");
+		System.out.println("===== 개인조회 ======");
+		System.out.println( mem.info() );
+		
+		// 목록 조회
+//		System.out.println("==== 목록조회:" + "'석' 포함된 ====");
+//		List<MemberVO> mList  = mDao.getMemberList("석"); 
+//		for (MemberVO memberVO : mList) {
+//			System.out.println(  memberVO.info() );
+//		}
+		
+		System.out.println("==== 목록조회:" + "'석' 포함된 ====");
+		Vector    mList  = mDao.getMemberList("석"); 
+		for (int i = 0; i < mList.size(); i++) {
+			Vector v = (Vector) mList.get(i);
+			for (int j = 0; j < v.size(); j++) {
+				System.out.print( (String) v.get( j ) +  " " );
+			}
+			System.out.println();
 		}
+
+		
 	}
+
 }
--- workspace/DBSwingMVCPrj/src/com/pknu/member/vo/MemberVO.java
+++ workspace/DBSwingMVCPrj/src/com/pknu/member/vo/MemberVO.java
@@ -1,18 +1,17 @@
 package com.pknu.member.vo;
 
 public class MemberVO {
-	private String id;
-	private String pwd;
-	private String name;
-	private String job;
-	private String gender;
-	private String intro ;
-	private String regdate;
-
+	// Fields
+	private String  id;
+	private String  pwd;
+	private String  name;
+	private String  job;
+	private String  gender;
+	private String  intro;
+	private String  regdate;
 	
-	//constructor
+	// Constructor
 	public MemberVO(String id, String pwd, String name, String job, String gender, String intro, String regdate) {
-		super();
 		this.id = id;
 		this.pwd = pwd;
 		this.name = name;
@@ -21,118 +20,108 @@ public class MemberVO {
 		this.intro = intro;
 		this.regdate = regdate;
 	}
-	
+
 	public MemberVO(String[] cols) {
-		this.id = cols[0];
-		this.pwd =cols[1];
-		this.name = cols[2];
-		this.job = cols[3];
-		this.gender = cols[4];
-		this.intro = cols[5];
+		this.id      = cols[0];
+		this.pwd     = cols[1];
+		this.name    = cols[2];
+		this.job     = cols[3];
+		this.gender  = cols[4];
+		this.intro   = cols[5];
 		this.regdate = cols[6];
 	}
-	
-	//getter /setter
+
+	public MemberVO(String id, String pwd, String name, String job, String gender, String intro) {
+		this.id = id;
+		this.pwd = pwd;
+		this.name = name;
+		this.job = job;
+		this.gender = gender;
+		this.intro = intro;	
+	}
+
+	// Getter Setter
 	public String getId() {
 		return id;
 	}
-
-
 	public void setId(String id) {
 		this.id = id;
 	}
 
-
 	public String getPwd() {
 		return pwd;
 	}
-
-
 	public void setPwd(String pwd) {
 		this.pwd = pwd;
 	}
 
-
 	public String getName() {
 		return name;
 	}
-
-
 	public void setName(String name) {
 		this.name = name;
 	}
 
-
 	public String getJob() {
 		return job;
 	}
-
-
 	public void setJob(String job) {
 		this.job = job;
 	}
 
-
 	public String getGender() {
 		return gender;
 	}
-
-
 	public void setGender(String gender) {
 		this.gender = gender;
 	}
 
-
 	public String getIntro() {
 		return intro;
 	}
-
-
 	public void setIntro(String intro) {
 		this.intro = intro;
 	}
 
-
 	public String getRegdate() {
 		return regdate;
 	}
-
-
 	public void setRegdate(String regdate) {
 		this.regdate = regdate;
 	}
-	//tostring
+
+	// toSting
 	@Override
 	public String toString() {
 		return "MemberVO [id=" + id + ", pwd=" + pwd + ", name=" + name + ", job=" + job + ", gender=" + gender
 				+ ", intro=" + intro + ", regdate=" + regdate + "]";
 	}
-	//method
 
+	// method
 	public String info() {
-		String fmt ="%s %s %s %s %s %s %s";
-		String genderKor = "";
-		switch (gender) {
-		case "M":
-			genderKor = "남자";
-			break;
-		case "F":
-			genderKor = "여자";
-			break;
-		default:
-			genderKor = "성별모름";
-			break;
+		String  fmt        =  "%s %s %s %s %s %s %s";		
+		String  genderKor  =  "";
+		
+		switch( gender) {
+		case "M": genderKor =  "남자";     break;
+		case "F": genderKor =  "여자";     break;
+		default : genderKor =  "성별모름";
 		}
-		String rdate = regdate.substring(0,10);
-		String msg = String.format(fmt, id, pwd, name, job, genderKor, intro, regdate);
-		return msg;
+		//         012345678901234567890
+		// regdate 2020-05-28 09:51:03
+		String  rdate  =  regdate.substring(0, 10);
+		String  msg  =  String.format(fmt, 
+				id,  pwd,  name, job, genderKor, intro, rdate);
+		return  msg;
 	}
-	
-	public String listinfo() {
-		String fmt ="%s %s %s %s %s %s %s";
-		String msg = String.format(fmt, id, pwd, name, job, gender, intro, regdate);
-		return msg;
+
+	public String  listInfo() {
+		String  fmt  =  "%s %s %s %s %s %s %s";
+		String  msg  =  String.format(fmt, 
+				id,  pwd,  name, job, gender, intro, regdate);
+		return  msg;
 	}
+
 	
 	
 }
