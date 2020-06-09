ef11f2ce0efa2028e1811b6464388287345b60e8
sh6137 <33920015+sh6137@users.noreply.github.com>
2020-06-09 20:44:55 +0900

    dasf

--- workspace/swingproject/src/swingproject/test2.java
+++ workspace/swingproject/src/swingproject/test2.java
@@ -3,9 +3,11 @@ package swingproject;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
-import net.miginfocom.swing.MigLayout;
+import javax.swing.JPanel;
+import javax.swing.JLabel;
+import java.awt.Button;
 
-public class test1 {
+public class test2 {
 
 	private JFrame frame;
 
@@ -16,7 +18,7 @@ public class test1 {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					test1 window = new test1();
+					test2 window = new test2();
 					window.frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -28,7 +30,7 @@ public class test1 {
 	/**
 	 * Create the application.
 	 */
-	public test1() {
+	public test2() {
 		initialize();
 	}
 
@@ -39,6 +41,14 @@ public class test1 {
 		frame = new JFrame();
 		frame.setBounds(100, 100, 450, 700);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("New label");
+		lblNewLabel.setBounds(122, 58, 188, 95);
+		frame.getContentPane().add(lblNewLabel);
+		
+		Button button = new Button("New button");
+		button.setBounds(122, 159, 188, 57);
+		frame.getContentPane().add(button);
 	}
-
 }
